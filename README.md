26f3cbfdbe0cd7a81b7ee71948f1384b3ebea62d
Sam Wolcott <samwolcott@gmail.com>
2026-08-14 13:22:51 -0700

    Initial Astro site and GitHub Pages deployment

---
+++ README.md
@@ -0,0 +1,18 @@
+# Hillary Faye Design
+
+Astro website for [hillaryfayedesign.com](https://hillaryfayedesign.com).
+
+## Local development
+
+```sh
+npm install
+npm run dev
+```
+
+## Production build
+
+```sh
+npm run build
+```
+
+Pushes to `main` automatically build and deploy the site through GitHub Pages.
---
+++ src/data/services.ts
@@ -0,0 +1,8 @@
+export const serviceGroups = [
+  { name: 'Luxurious combos', items: [['Root touch up / cut & style','$175'],['Color / cut & style','$197'],['Facial dimensional blonding / cut & style','$197'],['Partial dimensional blonding / cut & style','$230'],['Luxury dimension blonding / cut & style','$275'],['Celebrity dimensions blonding / cut & style','$350'],['Barber cut & camo','$105']] },
+  { name: 'Luxurious color', items: [['Root touch up (½–1 inch of grow out)','$100'],['Color','$122']] },
+  { name: 'Cuts & styling', items: [['Cut & style','$75'],['Barber / clipper haircut','$65']] },
+  { name: 'Blonding & dimensional color', items: [['Color correction','$200'],['Facial dimensions blonding','$145'],['Partial dimension blonding','$155'],['Luxury dimension blonding','$200'],['Celebrity dimensions blonding','$275']] },
+  { name: 'Extensions', items: [['Sew-in application','$430'],['Tape-in application','$373'],['Tape removal, retape & application','$302']] },
+  { name: 'Add-on services', items: [['Olaplex treatment','$30'],['Pureology mask','$30'],['HeatCure','$30'],['Additional color','$44'],['Gloss / toner with color service','$38'],['Brow maintenance','$26'],['Brow tinting & wax','$62'],['Lip maintenance','$18'],['Chin maintenance','$18']] },
+];
